47e154baaac9020cf9a9babce6c606bb4916f1c7
0x4d6165 <maeborow@posteo.net>
2023-04-28 15:58:41 -0700

    new stuff

---
+++ user-overrides.js
@@ -0,0 +1,6 @@
+user_pref("media.eme.enabled", true);
+user_pref("browser.startup.page", 3);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.resistFingerprinting", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("webgl.disabled", false);
